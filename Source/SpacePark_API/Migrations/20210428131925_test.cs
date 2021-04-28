6a8d242f16105ed01a183472cc3739b7abfdb76d
Kristian Jimmefors <kristian.jimmefors@gmail.com>
2021-04-28 15:31:55 +0200

    Initial commit, Added docker container with sql server

---
+++ Source/SpacePark_API/Migrations/20210428131925_test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpacePark_API.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Park",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpaceShip = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArrivalTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Payed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Park", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Park");
+        }
+    }
+}
---
+++ Source/SpacePark_API/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePark_API
+{
+    public class MyContext : DbContext
+    {
+        public virtual DbSet<Park> Park { get; set; }
+
+        public MyContext() : base()
+        {
+
+        }
+
+        //Byt connection string så det funkar för dig
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost,41433; Database=SpacePortDB; User ID=SA; Password=verystrong!pass123");
+        }
+    }
+}
